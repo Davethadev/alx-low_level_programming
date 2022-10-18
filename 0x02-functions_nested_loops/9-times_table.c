c9df95168c1496da41d8257ab6fc2286e4c50ac3
David Umanah <david_umanah@yahoo.com>
2022-10-18 16:34:29 -0700

    fix program error

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,11 +7,11 @@ void times_table(void)
 {
 	int num1, num2, product;
 
-	for (num1 = 0; num1 < 10; num1++)
+	for (num1 = 0; num1 <= 9; num1++)
 	{
 		_putchar('0');
 
-		for (num2 = 0; num2 < 10; num2++)
+		for (num2 = 0; num2 <= 9; num2++)
 		{
 			_putchar(',');
 			_putchar(' ');
