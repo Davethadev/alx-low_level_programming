a087ac2b00433f7749316f50ef3d48cf230b6c34
David Umanah <david_umanah@yahoo.com>
2022-10-21 15:26:59 -0700

    fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,10 +10,9 @@ void print_most_numbers(void)
 
 	for (y = '0'; y <= '9'; y++)
 	{
-		if (y == 2 || y == 4)
+		if (y == '2' || y == '4')
 			continue;
-		else
-			_putchar(y);
+		_putchar(y);
 	}
 
 	_putchar('\n');
