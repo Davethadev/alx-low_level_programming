1441c0f7c73fe819e45f8359a479476703dd9b7f
David Umanah <david_umanah@yahoo.com>
2022-10-21 15:11:28 -0700

    add program prints the numbers, from 0 to 9

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9
+ * Return: Returns void
+*/
+
+void print_most_numbers(void)
+{
+	int y;
+
+	for (y = '0'; y <= '9'; y++)
+	{
+		if (y == 2 || y == 4)
+			continue;
+		else
+			_putchar(y);
+	}
+
+	_putchar('\n');
+}
